ecdf879dc43e655288d4879934ccef9ed11ff9b9
jbreu <20864561+jbreu@users.noreply.github.com>
2025-02-09 11:33:57 +0100

    initial version printing on console

---
+++ retrieve_data.sh
@@ -0,0 +1,4 @@
+mkdir -p input
+curl -o input/bmz-iati-export.xml https://www.transparenzportal.bund.de/api/v1/activities/download/xml/bmz-iati-export.xml
+curl -o input/DE-1-Ressorts_R.xml https://teamwork.bmz.de/pub/bscw.cgi/9134919/DE-1-Ressorts_R.xml
+curl -o input/DE-1-Ressorts_C.xml https://teamwork.bmz.de/pub/bscw.cgi/9134919/DE-1-Ressorts_C.xml
